155281f072f0548b881cc2b725c4af48a20680e3
jakub <jakubzwardon@gmail.com>
2020-04-18 14:42:31 +0200

    Add office_rez.js file

--- includes/header.php
+++ includes/header.php
@@ -18,6 +18,7 @@ require 'config/config.php';
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="assets/js/bootstrap.js"></script>
 	<script src="assets/js/bootbox.min.js"></script>
+	<script src="assets/js/office_rez.js"></script>
 	
 	
 </head>
