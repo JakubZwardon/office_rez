bfc3a5f3dc338eb1f590dcfe714c48aab47d848c
jakub <jakubzwardon@gmail.com>
2020-04-19 21:45:50 +0200

    Add AJAX request for new reservation

--- assets/js/office_rez.js
+++ assets/js/office_rez.js
@@ -36,4 +36,31 @@ $(document).ready(function() {
         });
         return false;
     });
+
+    //submit add reservation button
+    $('#new_reservation_modal_form').submit(function() {
+        debugger;
+        $.ajax({
+            method: "POST",
+            url: "includes/handlers/ajax_add_new_reservation.php",
+            data: $('form.new_reservation_date_form').serialize(),
+            success: function(msg) {
+                if(msg == "BadWsName") {
+                    debugger;
+                    $('#add_res_ws_name_invalid').html("Proszę wprowadź poprawną nazwę stanowiska!");
+                } else if(msg == "DateTaken") {
+                    debugger;
+                    $('#add_res_date_invalid').html("Podany termin jest zajęty!");
+                } else {
+                    debugger;
+                    $("#new_reservation_modal").modal('hide');
+                    location.reload();
+                }
+            },
+            error: function() {
+                alert('failure');
+            }
+        });
+        return false;
+    });
 });
