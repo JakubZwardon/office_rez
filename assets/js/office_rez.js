a35fff891900b65467b12b51b0411c30e64b3262
jakub <jakubzwardon@gmail.com>
2020-04-20 11:14:09 +0200

    remove debugger

--- assets/js/office_rez.js
+++ assets/js/office_rez.js
@@ -41,26 +41,20 @@ $(document).ready(function() {
     $('#new_reservation_modal_form').submit(function() {
         $('#add_res_ws_name_invalid').html("");
         $('#add_res_date_invalid').html("");
-        debugger;
         $.ajax({
             method: "POST",
             url: "includes/handlers/ajax_add_new_reservation.php",
             data: $('form.new_reservation_date_form').serialize(),
             success: function(msg) {
                 if(msg == "BadWsName") {
-                    debugger;
                     $('#add_res_ws_name_invalid').html("Proszę wprowadź poprawną nazwę stanowiska!");
                 } else if(msg == "DateTaken") {
-                    debugger;
                     $('#add_res_date_invalid').html("Podany termin jest zajęty!");
                 } else if(msg == "DateInvalid") {
-                    debugger;
                     $('#add_res_date_invalid').html("Końcowa data nie może być wcześniejsza niż początkowa!");
                 } else if(msg == "DateLess") {
-                    debugger;
                     $('#add_res_date_invalid').html("Nie cofniesz się chyba w czasie:)!");
                 } else {
-                    debugger;
                     $("#new_reservation_modal").modal('hide');
                     location.reload();
                 }
